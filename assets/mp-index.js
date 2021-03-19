5fd5723334141aca11cd7a6e7eb0dd1abb90cc8e
user-one <rafa_zanetti_c@hotmail.com>
2021-03-19 15:14:34 -0300

    implantando comunicação com a API MP Checkout PRO

--- assets/mp-index.js
+++ assets/mp-index.js
@@ -81,10 +81,10 @@ for (var i = 0; i < numButtons; i++) {
   //updatePrice();  
   
   //voltar
-  /*document.getElementById("go-back").addEventListener("click", function() {
+  document.getElementById("go-back").addEventListener("click", function() {
     $(".container_payment").fadeOut(500);
     setTimeout(() => {
         $(".shopping-cart").show(500).fadeIn();
     }, 500);
     $('#checkout-btn').attr("disabled", false);  
-  });*/
+  });
