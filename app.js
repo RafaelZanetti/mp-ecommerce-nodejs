e05e88b1c19a881ab9302ba7617abc5732ee0f3a
user-one <rafa_zanetti_c@hotmail.com>
2021-03-19 15:22:31 -0300

    implantando comunicação com a API MP Checkout PRO

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/', function (req, res) {
 //MP Integration
 app.post("/create_preference", (req, res) => {
 
-    console.log(req.query);
+    console.log('log_PREFERENCE', req.body);
 
 	let preference = {
         collector_id: 725736327,
