47110189ef514224ec42f562f5160ef99ebbe9d9
user-one <rafa_zanetti_c@hotmail.com>
2021-03-19 09:56:11 -0300

    testes primarios

---
+++ js/index.js
@@ -0,0 +1,84 @@
+//Manipula a chamada para o back-end e gera preferência.
+document.getElementById("checkout-btn").addEventListener("click", function() {
+
+    $('.mercadopago-button').attr("disabled", true);
+    
+    description: as-producttile-tilelink.span
+
+
+    var orderData = {
+        quantity: document.getElementsByName("unit").value,
+        description: document.getElementsByName("title").value,
+        price: document.getElementsByName("price").value
+
+      //quantity: document.getElementById("quantity").value,
+      //description: document.getElementById("product-description").innerHTML,
+      //price: document.getElementById("unit-price").innerHTML
+    };
+      
+    fetch("/create_preference", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(orderData),
+      })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(preference) {
+            createCheckoutButton(preference.id);
+            $(".shopping-cart").fadeOut(500);
+            setTimeout(() => {
+                $(".container_payment").show(500).fadeIn();
+            }, 500);
+        })
+        .catch(function() {
+            alert("Unexpected error");
+            $('#checkout-btn').attr("disabled", false);
+        });
+  });
+  
+  //Criar preferência ao clicar no botão checkout
+  function createCheckoutButton(preference) {
+    var script = document.createElement("script");
+    
+     //O domínio de origem deve ser preenchido de acordo com o site ao qual você está se integrando.
+     // Por exemplo: para a Argentina ".com.ar" ou para o Brasil ".com.br".
+    script.src = "https://www.mercadopago.com.br/integrations/v1/web-payment-checkout.js";
+    script.type = "text/javascript";
+    script.dataset.preferenceId = preference;
+    document.getElementById("button-checkout").innerHTML = "";
+    document.querySelector("#button-checkout").appendChild(script);
+  }
+  
+  //Lidar com atualização de preço
+  function updatePrice() {
+    let quantity = document.getElementById("quantity").value;
+    let unitPrice = document.getElementById("unit-price").innerHTML;
+    let amount = parseInt(unitPrice) * parseInt(quantity);
+  
+    let description = document.getElementById("product-description").innerHTML;
+    
+    let elQuantity = document.querySelector("#summary-quantity");
+    document.querySelector(".item-name").innerHTML = description + " x ";
+    document.querySelector(".item-name").appendChild(elQuantity);
+  
+    document.getElementById("cart-total").innerHTML = "R$ " + amount;
+    document.getElementById("summary-price").innerHTML = "R$ " + unitPrice;
+    document.getElementById("summary-quantity").innerHTML = quantity;
+    document.getElementById("summary-total").innerHTML = "R$ " + amount;
+  
+    
+  }
+  document.getElementById("quantity").addEventListener("change", updatePrice);
+  updatePrice();  
+  
+  //voltar
+  document.getElementById("go-back").addEventListener("click", function() {
+    $(".container_payment").fadeOut(500);
+    setTimeout(() => {
+        $(".shopping-cart").show(500).fadeIn();
+    }, 500);
+    $('#checkout-btn').attr("disabled", false);  
+  });
