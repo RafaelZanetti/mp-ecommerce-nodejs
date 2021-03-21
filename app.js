af108c148fbc596e201bcb15a4ca8d3ead9cac8e
rafael.zanetti <rafa_zanetti_c@hotmail.com>
2021-03-20 21:49:10 -0300

    integrando MP Checkout PRO

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 var port = process.env.PORT || 3000
 
+const mercadopago = require("mercadopago");
+
+mercadopago.configurations.setAccessToken("APP_USR-334491433003961-030821-12d7475807d694b645722c1946d5ce5a-725736327"); 
+
 var app = express();
  
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static('assets'));
  
 app.use('/assets', express.static(__dirname + '/assets'));
@@ -15,8 +21,162 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
-app.get('/detail', function (req, res) {
+const details_order = [];
+
+app.get('/detail', (req, res) => {
     res.render('detail', req.query);
+
+    details_order.push(req.query);
+     
+    console.log(details_order);
 });
+
+//*--------------------//
+//MP Integration
+app.post("/create_preference", (req, res) => {
+
+    console.log('log_PREFERENCE', req.body);
+
+	let preference = {
+        collector_id: 725736327,
+		items: [{
+            id: 1234,
+			title: req.body.description,
+            description: "Celular de Tienda e-commerce",
+            picture_url: "https://www.collinsdictionary.com/images/full/book_181404689_1000.jpg",
+            category_id: "art",
+            currency_id: "BRL",
+			unit_price: Number(req.body.price),
+			quantity: Number(req.body.quantity),
+		}],
+        payer: {
+            name: "Lalo",
+            surname: "Landa",
+            email: "test_user_92801501@testuser.com",
+            date_created: "",
+            phone: {
+                area_code: "55",
+                number: 985298743
+            },
+            identification: {
+                type: "CPF",
+                number: "19191919100"
+            },
+            address: {
+                street_name: "Insurgentes Sur",
+                street_number: 1602,
+                zip_code: "78134-190"
+            }
+        },        
+		back_urls: {
+			"success": "http://localhost:8080/pages/success/index.html?feedback",
+			"failure": "http://localhost:8080/pages/failure/index.html?feedback",
+			"pending": "http://localhost:8080/pages/pending/index.html?feedback"
+		},
+        notification_url: "https://webhook.site/31d8362e-f603-476d-ae87-30e1648e22b4",
+        statement_descriptor: "MERCADOPAGO",
+        external_reference: "rafa_zanetti_c@hotmail.com",
+		auto_return: 'approved',
+	};
+
+    console.log(preference);
+
+	mercadopago.preferences.create(preference)
+		.then(function (response) {
+			res.json({id :response.body.id})
+		}).catch(function (error) {
+			console.log(error);
+		});
+});
+
+app.get('/feedback', function(request, response) {
+    response.json({
+       Payment: request.query.payment_id,
+       Status: request.query.status,
+       MerchantOrder: request.query.merchant_order_id
+   })
+});
+//*-----------------------------------//
+
+
+
+
+/**----------------------------------------- */
+//MP Integration
+
+/*mercadopago.configurations.setAccessToken("APP_USR-334491433003961-030821-12d7475807d694b645722c1946d5ce5a-725736327"); 
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("../../client"));
+
+app.get("/", function (req, res) {
+  res.status(200).sendFile("index.html");
+}); 
+
+app.post("/create_preference", (req, res) => {
+
+	let preference = {
+        collector_id: 725736327,
+		items: [{
+            id: 1234,
+			title: req.body.description,
+            description: "Celular de Tienda e-commerce",
+            picture_url: "https://www.collinsdictionary.com/images/full/book_181404689_1000.jpg",
+            category_id: "art",
+            currency_id: "BRL",
+			unit_price: Number(req.body.price),
+			quantity: Number(req.body.quantity),
+		}],
+        payer: {
+            name: "Lalo",
+            surname: "Landa",
+            email: "test_user_92801501@testuser.com",
+            date_created: "",
+            phone: {
+                area_code: "55",
+                number: 985298743
+            },
+            identification: {
+                type: "CPF",
+                number: "19191919100"
+            },
+            address: {
+                street_name: "Insurgentes Sur",
+                street_number: 1602,
+                zip_code: "78134-190"
+            }
+        },        
+		back_urls: {
+			"success": "http://localhost:8080/pages/success/index.html?feedback",
+			"failure": "http://localhost:8080/pages/failure/index.html?feedback",
+			"pending": "http://localhost:8080/pages/pending/index.html?feedback"
+		},
+        notification_url: "https://webhook.site/31d8362e-f603-476d-ae87-30e1648e22b4",
+        statement_descriptor: "MERCADOPAGO",
+        external_reference: "rafa_zanetti_c@hotmail.com",
+		auto_return: 'approved',
+	};
+
+    console.log(preference);
+
+	mercadopago.preferences.create(preference)
+		.then(function (response) {
+			res.json({id :response.body.id})
+		}).catch(function (error) {
+			console.log(error);
+		});
+});
+
+app.get('/feedback', function(request, response) {
+	 response.json({
+		Payment: request.query.payment_id,
+		Status: request.query.status,
+		MerchantOrder: request.query.merchant_order_id
+	})
+});*/
+
+/**----------------------------------------- */
+
 
 app.listen(port);
---
+++ assets/mp-index.js
@@ -0,0 +1,93 @@
+//Manipula a chamada para o back-end e gera preferência.
+var elMpButton = document.getElementsByClassName('mercadopago-button');
+var numButtons = elMpButton.length;
+
+//console.log(elMpButton);
+console.log('numeroButtonPagamento: ', numButtons);
+
+for (var i = 0; i < numButtons; i++) {
+    elMpButton[i].addEventListener('click', function() {
+    
+        //alert("entrou no click");
+        //$('.mercadopago-button').attr("disabled", true);
+
+        var orderData = {
+            description: document.getElementById('details--title').innerHTML,
+            quantity: document.getElementById('details--unit').innerHTML,
+            price: document.getElementById('details--price').innerHTML
+        };
+
+        console.log('orderDATA-LOG: ', orderData);
+        
+        /*fetch("/create_preference", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(orderData),
+        })
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(preference) {
+                createCheckoutButton(preference.id);
+                $(".shopping-cart").fadeOut(500);
+                setTimeout(() => {
+                    $(".container_payment").show(500).fadeIn();
+                }, 500);
+            })
+            .catch(function() {
+                alert("Unexpected error");
+                $('.mercadopago-button').attr("disabled", false);
+            });*/
+    });
+}
+
+/*  //Criar preferência ao clicar no botão checkout
+  function createCheckoutButton(preference) {
+    var script = document.createElement("script");
+    
+     //O domínio de origem deve ser preenchido de acordo com o site ao qual você está se integrando.
+     // Por exemplo: para a Argentina ".com.ar" ou para o Brasil ".com.br".
+    script.src = "https://www.mercadopago.com.br/integrations/v1/web-payment-checkout.js";
+    script.type = "text/javascript";
+    script.dataset.preferenceId = preference;
+    document.getElementsByClassName("mercadopago-button").innerHTML = "";
+    document.querySelector(".mercadopago-button").appendChild(script);
+  }
+  
+  //Lidar com atualização de preço
+  function updatePrice() {
+    let quantity = (document.getElementsByName("unit").value);
+    let unitPrice = document.getElementsByName("price").value;
+    //let amount = parseInt(unitPrice) * parseInt(quantity);
+
+  
+    let description = document.getElementsByName("title").value;
+    
+    //let elQuantity = document.querySelector("#summary-quantity");
+    document.querySelector(".details--title").innerHTML = description;
+    //document.querySelector(".item-name").appendChild(elQuantity);
+  
+    //document.getElementById("cart-total").innerHTML = "R$ " + amount;
+    document.querySelector('.details--price').innerHTML = 'R$ ' + unitPrice;
+    document.querySelector('.details--unit').innerHTML = quantity;
+    //document.getElementById("summary-total").innerHTML = "R$ " + amount;
+  
+    
+  }
+
+  const elQuantity = document.getElementsByClassName('details--unit');
+  console.log(elQuantity.length);
+
+  //document.getElementsByClassName('details--unit').addEventListener('change', updatePrice);
+  //updatePrice();  
+  
+  //voltar
+  document.getElementById("go-back").addEventListener("click", function() {
+    $(".container_payment").fadeOut(500);
+    setTimeout(() => {
+        $(".shopping-cart").show(500).fadeIn();
+    }, 500);
+    $('#checkout-btn').attr("disabled", false);  
+  });*/
