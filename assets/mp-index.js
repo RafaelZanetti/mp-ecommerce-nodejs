8d0a12e5b16ccc771138fda597d47f36e47dda69
user-one <rafa_zanetti_c@hotmail.com>
2021-03-23 10:16:47 -0300

    ajustes de segurança

--- assets/mp-index.js
+++ assets/mp-index.js
@@ -4,6 +4,9 @@ var numButtons = elMpButton.length;
 for (var i = 0; i < numButtons; i++) {
     elMpButton[i].addEventListener('click', function() {
     
+        const script = document.querySelector('script');
+        script.setAttribute('view', 'checkout');
+
         var orderData = {
             description: document.getElementById('details--title').innerHTML,
             quantity: document.getElementById('details--unit').innerHTML,
@@ -11,11 +14,16 @@ for (var i = 0; i < numButtons; i++) {
             img: document.getElementById('details--img').src
         };
         
+        const device_id = MP_DEVICE_SESSION_ID;
+
+        console.log('ID armor',device_id);
+
         fetch("/create_preference", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                     "integrator_id": "dev_24c65fb163bf11ea96500242ac130004",
+                    "X-meli-session-id": device_id,
                 },
                 body: JSON.stringify(orderData),
         })
@@ -34,35 +42,35 @@ for (var i = 0; i < numButtons; i++) {
     });
 }
 
-  //preference create
-  function createCheckoutButton(preference) {
-    var script = document.createElement("script");
-    
-    script.src = "https://www.mercadopago.com.br/integrations/v1/web-payment-checkout.js";
-    script.type = "text/javascript";
-    script.dataset.preferenceId = preference;
-    script.dataset.buttonLabel = "Pague a compra";
-    document.querySelector('#button-checkout').innerHTML = "";
-    document.querySelector('#button-checkout').appendChild(script);
-  }
+    //preference create
+    function createCheckoutButton(preference) {
+        var script = document.createElement("script");
+
+        script.src = "https://www.mercadopago.com.br/integrations/v1/web-payment-checkout.js";
+        script.type = "text/javascript";
+        script.dataset.preferenceId = preference;
+        script.dataset.buttonLabel = "Pague a compra";
+        document.querySelector('#button-checkout').innerHTML = "";
+        document.querySelector('#button-checkout').appendChild(script);
+    }
   
-  function resumeCheckout() {
-    let quantity = document.getElementById('details--unit').innerHTML;
-    let unitPrice = document.getElementById('details--price').innerHTML
-    let amount = parseInt(unitPrice) * parseInt(quantity);
+    function resumeCheckout() {
+        let quantity = document.getElementById('details--unit').innerHTML;
+        let unitPrice = document.getElementById('details--price').innerHTML
+        let amount = parseInt(unitPrice) * parseInt(quantity);
 
-    let description = document.getElementById('details--title').innerHTML;
+        let description = document.getElementById('details--title').innerHTML;
 
-    $('.as-filter-button').hide();
-    $('.as-producttile-tilelink').hide();
-    $('#details--price').hide();
-    $('#details--unit').hide();    
+        $('.as-filter-button').hide();
+        $('.as-producttile-tilelink').hide();
+        $('#details--price').hide();
+        $('#details--unit').hide();    
 
-    $('.as-accessories-filter-tile').append('<h2>Resumo do Pedido</h2>');
+        $('.as-accessories-filter-tile').append('<h2>Resumo do Pedido</h2>');
 
-    $('.mini-gallery .as-producttile-image').css({ "width": "150px", "height": "auto", "margin-top": "20px"});
-    
-    document.getElementById('details--resume-title').innerHTML = `${description} x ${quantity}  -  R$ ${amount}`;
-    
-    $("#button-checkout").append( "<h3>" + "Total  R$: " + amount +"</h3>" );
-  }
+        $('.mini-gallery .as-producttile-image').css({ "width": "150px", "height": "auto", "margin-top": "20px"});
+
+        document.getElementById('details--resume-title').innerHTML = `${description} x ${quantity}  -  R$ ${amount}`;
+
+        $("#button-checkout").append( "<h3>" + "Total  R$: " + amount +"</h3>" );
+    }
